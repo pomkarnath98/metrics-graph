f7a505af931435fc8d87ff5f2aa0e7af4261001e
Omkarnath Parida <omkarnath@revvsales.com>
2021-04-13 17:21:51 +0530

    fix: Updated code for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const authRoute = require("./routes/auth");
 
+const PORT = process.env.PORT || 9000;
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -11,21 +13,28 @@ dotenv.config();
 app.use(cors());
 
 mongoose.connect(
-    process.env.ATLAS_URI,
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true
-    },
-    (err) => {
-        if (err) console.log(err)
-        console.log("The database is connected");
-    }
+  process.env.MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) console.log(err);
+    console.log("The database is connected");
+  }
 );
-
 
 app.use("/api", authRoute);
 
-app.listen(9000, () => {
-    console.log("The server is running on port 9000");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
 });
